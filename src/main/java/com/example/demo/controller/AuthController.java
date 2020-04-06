e9e3b157116948b8de2c2c1c359e40ff0bd185a3
TC-0100 <63219023+TC-0100@users.noreply.github.com>
2020-04-06 14:08:08 +0900

    fix
    fix: user info is not displayed at top-page

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -1,24 +1,38 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.example.demo.repository.AccountRepository;
 
 @Controller
 public class AuthController {
 
 	@RequestMapping("/")
 	public String index() {
+		System.out.println(">>>>>200406 redirect top");
 		return "redirect:/top";
 	}
 
 	@RequestMapping(value = "/login")
 	public String login() {
+		System.out.println(">>>>>200406 method login");
 		return "login";
 	}
 
-	@RequestMapping("/top")
-	public String top() {
-		return "/top";
+	@Autowired
+	AccountRepository accRepo;
+
+	@RequestMapping(value = "/top", method = RequestMethod.GET)
+	public void getLoginUser(ModelMap model) {
+		System.out.println(">>>>>200406 method getLoginUser");
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		model.addAttribute("loginUser", accRepo.findByUsername(auth.getName()));
 	}
 
 }
